d61afded1ce5556c0c6c9c22f3d2bdb8472f92aa
刘松坡 <liusongpo@xzcysoft.com>
2018-03-09 13:35:22 +0800

    更新：
    1、添加注释

--- src/main/java/com/songpo/cache/BaseCache.java
+++ src/main/java/com/songpo/cache/BaseCache.java
@@ -15,8 +15,14 @@ import java.util.concurrent.TimeUnit;
 @AllArgsConstructor
 public class BaseCache<V> {
 
+    /**
+     * 缓存前缀
+     */
     public String prefix;
 
+    /**
+     * Redis模板
+     */
     public RedisTemplate<String, V> redisTemplate;
 
     /**
@@ -34,11 +40,23 @@ public class BaseCache<V> {
      */
     public Boolean limitTime = true;
 
+    /**
+     * 获取缓存
+     *
+     * @param key 键
+     * @return 值
+     */
     public V get(String key) {
         return redisTemplate.opsForValue().get(prefix + key);
     }
 
+    /**
+     * 添加缓存
+     * @param key 键
+     * @param value 值
+     */
     public void put(String key, V value) {
+        // 如果启用缓存周期限制，则根据设置的周期进行保持，否则不进行缓存周期设置
         if (limitTime) {
             redisTemplate.opsForValue().set(prefix + key, value, timeOut, timeUnit);
         } else {
@@ -46,10 +64,17 @@ public class BaseCache<V> {
         }
     }
 
+    /**
+     * 删除缓存
+     * @param key 键
+     */
     public void evict(String key) {
         redisTemplate.delete(prefix + key);
     }
 
+    /**
+     * 清空缓存
+     */
     public void clear() {
         redisTemplate.delete(prefix);
     }
