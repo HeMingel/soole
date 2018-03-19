65b10eab8530b55bf5d86d0b5c786ad5971b9877
刘松坡 <liusongpo@xzcysoft.com>
2018-03-19 15:59:11 +0800

    更新：
    1、将缓存层合并到common工程
    2、更新工程说明文档

--- searched-common/src/main/java/com/songpo/searched/config/RedisConfig.java
+++ searched-common/src/main/java/com/songpo/searched/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.songpo.searched.config;
 
+import com.songpo.searched.domain.MyShoppingCartPojo;
 import com.songpo.searched.entity.SlUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +12,27 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    /**
+     * redis 构造器
+     *
+     * @param factory
+     * @return
+     */
     @Bean
-    public RedisTemplate<String, Object> mybatisRedisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, MyShoppingCartPojo> myShoppingCartTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, MyShoppingCartPojo> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(MyShoppingCartPojo.class));
         redisTemplate.setConnectionFactory(factory);
         return redisTemplate;
     }
 
+    /**
+     * 用户缓存模板
+     *
+     * @param factory
+     * @return
+     */
     @Bean
     public RedisTemplate<String, SlUser> userRedisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, SlUser> redisTemplate = new RedisTemplate<>();
