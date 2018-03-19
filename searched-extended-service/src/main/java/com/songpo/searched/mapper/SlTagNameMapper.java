32e2982220adda79b1ec06dd36b632d4bf8b08ce
刘松坡 <liusongpo@xzcysoft.com>
2018-03-19 15:56:37 +0800

    更新：
    1、将缓存层合并到common工程
    2、更新工程说明文档

--- searched-extended-service/src/main/java/com/songpo/searched/mapper/SlTagNameMapper.java
+++
@@ -1,9 +0,0 @@
-package com.songpo.searched.mapper;
-
-import com.songpo.searched.entity.SlTag;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SlTagNameMapper extends Mapper<SlTag> {
-
-    String findTagNameByTagId(String tagId);
-}
