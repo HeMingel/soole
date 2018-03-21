feca1383477c30013f093e079b6a5a2a70f8d42c
wangxiaosenIFIFHDaIEaHFaHFaHFaHFaHGd:wq <you@example.com>
2018-03-21 19:20:42 +0800

    修改 1.修改预下单类 2.增加CMOrderDetail类

--- searched-api/src/main/java/com/songpo/searched/controller/OrderController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/OrderController.java
@@ -2,10 +2,14 @@ package com.songpo.searched.controller;
 
 
 import com.songpo.searched.domain.BusinessMessage;
+import com.songpo.searched.domain.CMSlOrderDetail;
 import com.songpo.searched.entity.SlOrder;
 import com.songpo.searched.entity.SlOrderDetail;
 import com.songpo.searched.service.OrderService;
+import com.sun.org.apache.regexp.internal.RE;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Api(description = "订单管理")
@@ -28,12 +33,25 @@ public class OrderController {
      * 预下单订单
      *
      * @param slOrder
-     * @param orderDetail
+     * @param cmSlOrderDetail
      * @return
      */
     @ApiOperation(value = "添加预下单")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "用户id", paramType = "form", required = true),
+            @ApiImplicitParam(name = "shippngAddressId", value = "地址Id", paramType = "form", required = true),
+            @ApiImplicitParam(name = "repositoryId", value = "店铺仓库唯一标识", paramType = "form", required = true),
+            @ApiImplicitParam(name = "quantity", value = "订单商品数量", paramType = "form", required = true),
+            @ApiImplicitParam(name = "paymentState", value = "支付状态(0：待支付1：支付成功 2：支付失败)", paramType = "form"),
+            @ApiImplicitParam(name = "type", value = "订单类型(1：普通订单2：拼团订单3:预售订单)", paramType = "form", required = true),
+            @ApiImplicitParam(name = "paymentChannel", value = "支付类型(1：微信支付 2：支付宝支付 3：厦门银行支付)", paramType = "form"),
+            @ApiImplicitParam(name = "payTime", value = "支付时间", paramType = "form"),
+            @ApiImplicitParam(name = "buyerMessage", value = "买家留言", paramType = "form"),
+            @ApiImplicitParam(name = "price", value = "单个商品的价格", paramType = "form"),
+            @ApiImplicitParam(name = "deductPulse", value = "单个商品需扣除的金豆", paramType = "form")
+    })
     @PostMapping("add")
-    public BusinessMessage addOrder(SlOrder slOrder, List<SlOrderDetail> orderDetail) {
-        return this.orderService.addOrder(slOrder, orderDetail);
+    public BusinessMessage addOrder(SlOrder slOrder, CMSlOrderDetail cmSlOrderDetail) {
+        return this.orderService.addOrder(slOrder, cmSlOrderDetail);
     }
 }
--- searched-api/src/main/java/com/songpo/searched/service/OrderService.java
+++ searched-api/src/main/java/com/songpo/searched/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.songpo.searched.service;
 
 import com.songpo.searched.domain.BusinessMessage;
+import com.songpo.searched.domain.CMSlOrderDetail;
 import com.songpo.searched.entity.SlOrder;
 import com.songpo.searched.entity.SlOrderDetail;
 import com.songpo.searched.entity.SlProductRepository;
@@ -35,7 +36,7 @@ public class OrderService {
      * @param orderDetail
      * @return
      */
-    public BusinessMessage addOrder(SlOrder slOrder, List<SlOrderDetail> orderDetail) {
+    public BusinessMessage addOrder(SlOrder slOrder, CMSlOrderDetail orderDetail) {
         Logger log = LoggerFactory.getLogger(SlOrderDetail.class);
         BusinessMessage message = new BusinessMessage();
         double money = 0.00;
@@ -47,12 +48,13 @@ public class OrderService {
                 }});
                 if (null != user) {
                     if (StringUtils.hasLength(slOrder.getShippngAddressId())) {
+                        slOrder.setId(UUID.randomUUID().toString());
                         slOrder.setSerialNumber(OrderNumGeneration.getOrderIdByUUId());// 生成订单编号
                         orderMapper.insertSelective(slOrder);
-                        for (SlOrderDetail slOrderDetail : orderDetail) {
-                            if (StringUtils.hasLength(slOrderDetail.getProductId())) {
+                        for (SlOrderDetail slOrderDetail : orderDetail.getSlOrderDetails()) {
+                            if (null != slOrderDetail.getRepositoryId()) {
                                 SlProductRepository repository = this.productRepositoryService.selectOne(new SlProductRepository() {{
-                                    setId(slOrderDetail.getProductId());
+                                    setId(slOrderDetail.getRepositoryId()); // 根据仓库ID 去查询商品的详细信息(选好规格的价格,金豆等等)
                                 }});
                                 if (null != repository && StringUtils.hasLength(slOrderDetail.getQuantity())) {
                                     money += repository.getPrice().doubleValue(); // 钱相加 用于统计和添加到订单表扣除总钱里边
@@ -62,7 +64,7 @@ public class OrderService {
                                         setOrderId(slOrder.getId()); // 订单ID
                                         setQuantity(slOrderDetail.getQuantity()); // 商品数量
                                         setPrice(repository.getPrice()); // 单个商品价格
-                                        setProductId(slOrderDetail.getProductId());// 商品ID
+                                        setProductId(repository.getProductId());// 商品ID
                                         setShopId(repository.getShopId());// 店铺唯一标识
                                         setRepositoryId(repository.getId()); // 店铺仓库ID
                                         setDeductPulse(repository.getPulse()); // 扣除单个商品了豆数量
@@ -81,6 +83,9 @@ public class OrderService {
                             setTotalAmount(amount);
                             setDeductTotalPulse(totalPulse);
                         }}, example);
+                        message.setData(1);
+                        message.setSuccess(true);
+                        message.setMsg("创建订单成功");
                     } else {
                         message.setMsg("收货地址不能为空");
                     }
