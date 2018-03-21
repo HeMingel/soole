ce42e7fd43c21acb79c872d63a528c418ab313f6
wangxiaosenIFIFHDaIEaHFaHFaHFaHFaHGd:wq <you@example.com>
2018-03-21 19:23:55 +0800

    新增 CMSlOrderDetail.java

---
+++ searched-common/src/main/java/com/songpo/searched/domain/CMSlOrderDetail.java
@@ -0,0 +1,13 @@
+package com.songpo.searched.domain;
+
+import com.songpo.searched.entity.SlOrderDetail;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CMSlOrderDetail {
+
+    private List<SlOrderDetail> slOrderDetails;
+
+}
