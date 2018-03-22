44355ec6e97c97dd1148904cdbd68aeacb6c98f9
yuehui <yuehui@xzcysoft.com>
2018-03-22 18:15:18 +0800

    更新:   1:完善注释

--- searched-api/src/main/java/com/songpo/searched/service/CmProductCommentService.java
+++ searched-api/src/main/java/com/songpo/searched/service/CmProductCommentService.java
@@ -33,6 +33,7 @@ public class CmProductCommentService {
      * @return
      */
     public BusinessMessage findGoodsCommentsByGoodsId(String goodsId,Integer status,Integer page, Integer size){
+        log.debug("查询 商品Id:{},评论好中差:{},页数:{},条数:{}",goodsId,status,page,size);
         BusinessMessage businessMessage = new BusinessMessage();
         businessMessage.setSuccess(false);
         try {
