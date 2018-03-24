81305f0d22f5e58634ecb521a91b5e1fcbbe27ce
刘松坡 <liusongpo@xzcysoft.com>
2018-03-24 18:14:25 +0800

    更新： 1、增加忘记密码接口

--- searched-api/src/main/java/com/songpo/searched/config/Oauth2Config.java
+++ searched-api/src/main/java/com/songpo/searched/config/Oauth2Config.java
@@ -61,6 +61,8 @@ public class Oauth2Config {
                     .antMatchers("/api/common/v1/system/register").permitAll()
                     // 不拦截短信接口
                     .antMatchers("/api/common/v1/sms/verify-code").permitAll()
+                    // 不拦截忘记密码接口
+                    .antMatchers("/api/common/v1/sms/forgot-password").permitAll()
                     // 不拦截商品相关接口
                     .antMatchers("/api/v2/**").permitAll()
                     // 其他请求都需要经过授权
