758c4525ea6eb6f73e082a9526dead72eacbcf8a
刘松坡 <liusongpo@xzcysoft.com>
2018-03-27 18:13:34 +0800

    更新： 1、同步数据库表结构

---
+++ searched-base/src/main/java/com/songpo/searched/mapper/SlSystemConnectorMapper.java
@@ -0,0 +1,7 @@
+package com.songpo.searched.mapper;
+
+import com.songpo.searched.entity.SlSystemConnector;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SlSystemConnectorMapper extends Mapper<SlSystemConnector> {
+}
