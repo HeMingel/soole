89e2cb07cf165130007f4b026d1df9fe5db11da3
yuehui <yuehui@xzcysoft.com>
2018-03-28 16:24:18 +0800

    更新:   1:商品分类

--- searched-api/src/main/java/com/songpo/searched/controller/ProductTypeController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/ProductTypeController.java
@@ -33,25 +33,15 @@ public class ProductTypeController {
     /**
      * 搜索一级分类
      *
-     * @param parentId 上级分类唯一标识符
      * @return 业务消息
      */
     @ApiOperation(value = "查询商品分类")
     @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "parentId", value = "上级分类唯一标识符", paramType = "query", required = true)
     })
     @GetMapping("list")
-    public BusinessMessage<List<Map<String, Object>>> findAll(String parentId) {
-        BusinessMessage<List<Map<String, Object>>> message = new BusinessMessage<>();
-        try {
-            message.setData(this.productTypeService.findAll(parentId));
-            message.setSuccess(true);
-        } catch (Exception e) {
-            log.error("查询商品分类失败，{}", e);
+    public BusinessMessage<List<Map<String, Object>>> findAll() {
 
-            message.setMsg("查询商品分类失败，请重试");
-        }
-        return message;
+        return this.productTypeService.findAll();
     }
 
 
--- searched-api/src/main/java/com/songpo/searched/service/CmProductTypeService.java
+++ searched-api/src/main/java/com/songpo/searched/service/CmProductTypeService.java
@@ -1,9 +1,16 @@
 package com.songpo.searched.service;
 
+import com.songpo.searched.domain.BusinessMessage;
+import com.songpo.searched.entity.SlProductType;
 import com.songpo.searched.mapper.CmProductTypeMapper;
+import com.songpo.searched.mapper.SlProductTypeMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,19 +18,40 @@ import java.util.Map;
  * @author 刘松坡
  */
 @Service
+@Slf4j
 public class CmProductTypeService {
 
     @Autowired
     private CmProductTypeMapper mapper;
+    @Autowired
+    private SlProductTypeMapper slProductTypeMapper;
 
     /**
      * 查询商品分类
      *
-     * @param parentId 上级分类唯一标识符
      * @return 商品分类集合
      */
-    public List<Map<String, Object>> findAll(String parentId) {
-        return this.mapper.selectAll(parentId);
+    public BusinessMessage findAll() {
+        BusinessMessage message = new BusinessMessage<>();
+        try {
+            List<Map<String,Object>> parentMenu = this.mapper.selectAll();
+            List list = new ArrayList();
+            for(int i=0;i<parentMenu.size();i++){
+                Map map = new HashMap();
+
+                List<Map<String,Object>> twoMenu = this.mapper.selectTwoMenu(parentMenu.get(i).get("id").toString());
+                map.put("parentMenu",parentMenu.get(i));
+                map.put("twoMenu",twoMenu);
+                list.add(map);
+            }
+            message.setData(list);
+            message.setSuccess(true);
+        } catch (Exception e) {
+            log.error("查询商品分类失败，{}", e);
+
+            message.setMsg("查询商品分类失败，请重试");
+        }
+        return message;
     }
 
 }
