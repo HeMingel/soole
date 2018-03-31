6ff180e29df7666cb2f720bad40742613cd277e8
yuehui <yuehui@xzcysoft.com>
2018-03-31 14:53:51 +0800

    更新:     1.商品模式添加

--- searched-api/src/main/java/com/songpo/searched/constant/SalesModeConstant.java
+++ searched-api/src/main/java/com/songpo/searched/constant/SalesModeConstant.java
@@ -6,6 +6,26 @@ package com.songpo.searched.constant;
 public class SalesModeConstant {
 
     /**
+     * 拼团
+     */
+    public static final int SALES_MODE_GROUP = 1;
+    /**
+     * 预售
+     */
+    public static final int SALES_MODE_PRESELL = 2;
+    /**
+     * 豆赚
+     */
+    public static final int SALES_MODE_BEANS = 3;
+    /**
+     * 消费返利
+     */
+    public static final int SALES_MODE_REBATE = 4;
+    /**
+     *一元购
+     */
+    public static final int SALES_MODE_ONE = 5;
+    /**
      * 销售模式-普通
      */
     public static final int SALES_MODE_NORMAL = 6;
