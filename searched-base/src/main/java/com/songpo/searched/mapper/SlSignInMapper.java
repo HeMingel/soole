b7cee8a8867456df7bc32e90fd69a4f61dff08bf
yuehui <yuehui@xzcysoft.com>
2018-04-06 15:34:56 +0800

    修改:     生成SlsignIn表

---
+++ searched-base/src/main/java/com/songpo/searched/mapper/SlSignInMapper.java
@@ -0,0 +1,7 @@
+package com.songpo.searched.mapper;
+
+import com.songpo.searched.entity.SlSignIn;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SlSignInMapper extends Mapper<SlSignIn> {
+}
