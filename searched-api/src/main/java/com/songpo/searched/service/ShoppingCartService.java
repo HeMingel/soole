d6f578ca520f6aebf8398f0249fa7765168dc16c
王晓森 <you@example.com>
2018-04-16 10:00:18 +0800

    修改 购物车接口

--- searched-api/src/main/java/com/songpo/searched/service/ShoppingCartService.java
+++ searched-api/src/main/java/com/songpo/searched/service/ShoppingCartService.java
@@ -39,7 +39,6 @@ public class ShoppingCartService {
      * 新增购物车
      *
      * @param pojo
-     * @param clientId
      * @return
      */
     public BusinessMessage addMyShoppingCart(CMShoppingCart pojo) {
@@ -90,7 +89,6 @@ public class ShoppingCartService {
     /**
      * 查询购物车
      *
-     * @param clientId
      * @return
      */
     public BusinessMessage findCart() {
@@ -139,7 +137,7 @@ public class ShoppingCartService {
                                             cmGoods.setShopName(slShop.getName());// 店铺名称
                                             cmGoods.setRemainingqty(repository.getCount());// 商品剩余数量 返回0的话 前台就显示失效
                                             cmGoods.setSoldOut(slProduct.getSoldOut());// 商品是否下架 true:已下架前台就显示失效  false:未下架
-                                            cmGoods.setRebatePulse(repository.getRebatePulse());// 纯金钱商品返了豆数量
+                                            cmGoods.setRebatePulse(repository.getPlaceOrderReturnPulse());// 纯金钱商品返了豆数量
                                             cmGoods.setMyBeansCounts(user.getCoin() + user.getSilver()); // 我剩余豆子总和金豆加银豆
                                             list1 = cart1.getCarts();
                                             list1.add(cmGoods);
@@ -167,7 +165,7 @@ public class ShoppingCartService {
                                             cmGoods.setShopName(slShop.getName());// 店铺名称
                                             cmGoods.setRemainingqty(repository.getCount());// 商品剩余数量 返回0的话 前台就显示失效
                                             cmGoods.setSoldOut(slProduct.getSoldOut());// 商品是否下架 true:已下架前台就显示失效  false:未下架
-                                            cmGoods.setRebatePulse(repository.getRebatePulse());// 纯金钱商品返了豆数量
+                                            cmGoods.setRebatePulse(repository.getPlaceOrderReturnPulse());// 纯金钱商品返了豆数量
                                             cmGoods.setMyBeansCounts(user.getCoin() + user.getSilver()); // 我剩余豆子总和金豆加银豆
                                             list2.add(cmGoods);
                                             cart2.setCarts(list2);
@@ -193,7 +191,7 @@ public class ShoppingCartService {
                                     cmGoods.setSpecificationName(repository.getProductDetailGroupName());// 查询组合规格名称
                                     cmGoods.setRemainingqty(repository.getCount());// 商品剩余数量 返回0的话 前台就显示失效
                                     cmGoods.setSoldOut(slProduct.getSoldOut());// 商品是否下架 true:已下架前台就显示失效  false:未下架
-                                    cmGoods.setRebatePulse(repository.getRebatePulse());// 纯金钱商品返了豆数量
+                                    cmGoods.setRebatePulse(repository.getPlaceOrderReturnPulse());// 纯金钱商品返了豆数量
                                     cmGoods.setMyBeansCounts(user.getCoin() + user.getSilver()); // 我剩余豆子总和金豆加银豆
                                     goodsList.add(cmGoods);
                                     cart.setShopId(slShop.getId());
