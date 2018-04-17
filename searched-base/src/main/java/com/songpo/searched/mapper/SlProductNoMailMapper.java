eed7dd25dce1d37fb35892a1c3908453748ec7a3
刘松坡 <liusongpo@xzcysoft.com>
2018-04-17 09:31:34 +0800

    更新： 1、同步最新数据库表结构

---
+++ searched-base/src/main/java/com/songpo/searched/mapper/SlProductNoMailMapper.java
@@ -0,0 +1,7 @@
+package com.songpo.searched.mapper;
+
+import com.songpo.searched.entity.SlProductNoMail;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SlProductNoMailMapper extends Mapper<SlProductNoMail> {
+}
---
+++ searched-base/src/main/java/com/songpo/searched/mapper/SlReturnsDetailMapper.java
@@ -0,0 +1,7 @@
+package com.songpo.searched.mapper;
+
+import com.songpo.searched.entity.SlReturnsDetail;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SlReturnsDetailMapper extends Mapper<SlReturnsDetail> {
+}
