d527680d341ae2ef9a048ad73505a6f4c1bdfc26
王晓森 <you@example.com>
2018-04-18 19:43:25 +0800

    修改 售后服务单

--- searched-api/src/main/java/com/songpo/searched/controller/AfterSalesController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/AfterSalesController.java
@@ -1,6 +1,4 @@
 package com.songpo.searched.controller;
-
-
 import com.songpo.searched.domain.BusinessMessage;
 import com.songpo.searched.entity.SlAfterSalesService;
 import com.songpo.searched.service.AfterSalesService;
@@ -27,12 +25,12 @@ public class AfterSalesController {
     @ApiImplicitParams({
             @ApiImplicitParam(value = "申请售后类型(1:退货退款 2:仅退款)", name = "type", paramType = "form", required = true),
             @ApiImplicitParam(value = "申请原因", name = "reason", paramType = "form", required = true),
-            @ApiImplicitParam(value = "退款金额", name = "money", paramType = "form", required = true),
+            @ApiImplicitParam(value = "退款金额", name = "money", paramType = "form"),
             @ApiImplicitParam(value = "凭证图片", name = "file", paramType = "form"),
-            @ApiImplicitParam(value = "商品id", name = "productId", paramType = "form", required = true),
-            @ApiImplicitParam(value = "店铺id", name = "shopId", paramType = "form", required = true),
+            @ApiImplicitParam(value = "商品id", name = "productId", paramType = "form"),
+            @ApiImplicitParam(value = "店铺id", name = "shopId", paramType = "form"),
             @ApiImplicitParam(value = "申请说明", name = "remark", paramType = "form"),
-            @ApiImplicitParam(value = "订单编号", name = "orderId", paramType = "form", required = true)
+            @ApiImplicitParam(value = "订单明细id", name = "orderDetailId", paramType = "form", required = true)
     })
     @PostMapping()
     public BusinessMessage insertAfterSales(SlAfterSalesService afterSalesService, MultipartFile[] files) {
