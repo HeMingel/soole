6c9695c73752902b84208430dea8df9dc5b86735
刘松坡 <liusongpo@xzcysoft.com>
2018-04-19 09:34:04 +0800

    更新： 1、调整支付通知逻辑

--- searched-api/src/main/java/com/songpo/searched/controller/PaymentController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/PaymentController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author 刘松坡
@@ -18,11 +17,32 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/api/common/v1/payment")
 public class PaymentController {
 
-    @Autowired
-    private PaymentService paymentService;
+    private final PaymentService paymentService;
 
-    @PostMapping("wx-notify")
-    public String wxPayNotify(HttpServletRequest request, HttpServletResponse response) {
-        return this.paymentService.wxNotify(request, response);
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    /**
+     * 微信支付支付通知处理接口
+     *
+     * @param request 请求参数
+     * @return 支付通知处理结果
+     */
+    @PostMapping("wxpay-notify")
+    public String wxPayNotify(HttpServletRequest request) {
+        return this.paymentService.wxPayNotify(request);
+    }
+
+    /**
+     * 支付宝支付通知处理接口
+     *
+     * @param request 请求参数
+     * @return 支付通知处理结果
+     */
+    @PostMapping("alipay-notify")
+    public String aliPayNotify(HttpServletRequest request) {
+        return this.paymentService.wxPayNotify(request);
     }
 }
