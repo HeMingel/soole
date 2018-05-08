74e57f2fb8f76c4f743ce53163fbdd8597ae52b8
刀之哀伤 <liusongpo@gmail.com>
2018-05-08 19:55:37 +0800

    1、调整微信支付返回参数

--- searched-common/src/main/kotlin/com/songpo/searched/wxpay/service/WxPayService.kt
+++ searched-common/src/main/kotlin/com/songpo/searched/wxpay/service/WxPayService.kt
@@ -5,6 +5,9 @@ import com.github.wxpay.sdk.WXPayConfig
 import com.songpo.searched.wxpay.config.WxPayConfigProperties
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 
 /**
  * 支付宝支付服务集成实现
@@ -275,15 +278,15 @@ class WxPayService(val config: WxPayConfigProperties) {
             data.put("scene_info", scene_info!!)
         }
 
-        result = wxpay.unifiedOrder(data)
+        val result = wxpay.unifiedOrder(data)
         if (result["return_code"] === "SUCCESS" && result["result_code"] === "SUCCESS") {
             return mapOf(
                     Pair("partnerId", this.config.mchId),
-                    Pair("prepayId", result["prepay_id"]),
+                    Pair("prepayId", result["prepay_id"] ?: ""),
                     Pair("package", "Sign=WXPay"),
-                    Pair("nonceStr", result["nonce_str"]),
-                    Pair("timeStamp", result["time_stamp"]),
-                    Pair("sign", result["sign"])
+                    Pair("nonceStr", result["nonce_str"] ?: ""),
+                    Pair("timeStamp", result["time_stamp"] ?: ""),
+                    Pair("sign", result["sign"] ?: "")
             )
         }
         return result
