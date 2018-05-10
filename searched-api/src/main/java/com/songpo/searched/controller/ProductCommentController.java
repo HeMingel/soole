862d29eb569eea3ff8322b23b6637aceef8e8ced
dzf <diaozf1326@163.com>
2018-05-10 15:56:22 +0800

    1、新增商品评论接口 2、修改获取商品评论接口

--- searched-api/src/main/java/com/songpo/searched/controller/ProductCommentController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/ProductCommentController.java
@@ -8,10 +8,10 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * @author Y.H
@@ -34,8 +34,20 @@ public class ProductCommentController {
             @ApiImplicitParam(name = "size", value = "每页数量", paramType = "form")
     })
     @GetMapping("goods-comments")
-    public BusinessMessage goodsComments (String goodsId,Integer status, Integer page, Integer size){
-        return this.productCommentService.findGoodsCommentsByGoodsId(goodsId,status,page,size);
+    public BusinessMessage goodsComments(String goodsId, Integer status, Integer page, Integer size) {
+        return this.productCommentService.findGoodsCommentsByGoodsId(goodsId, status, page, size);
+    }
+
+    @ApiOperation(value = "新增商品评论")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "productId", value = "商品ID", paramType = "form", required = true),
+            @ApiImplicitParam(name = "content", value = "评论内容", paramType = "form", required = true),
+            @ApiImplicitParam(name = "status", value = "评论状态:1好2中3差", paramType = "form", required = true),
+            @ApiImplicitParam(name = "imageList", value = "评论图片", paramType = "form"),
+    })
+    @PostMapping("add")
+    public BusinessMessage insertGoodsComments(String productId, String content, Integer status, List<MultipartFile> imageList) {
+        return productCommentService.insertGoodsComments(productId, content, status, imageList);
     }
 
 
