ef7c5cc7e7ffab98823ae5302eaad8fe2290a41d
王晓森 <you@example.com>
2018-05-18 17:57:57 +0800

    修复 定时任务

--- searched-api/src/main/java/com/songpo/searched/config/CommonConfig.java
+++ searched-api/src/main/java/com/songpo/searched/config/CommonConfig.java
@@ -6,10 +6,8 @@ import com.songpo.searched.entity.SlSignIn;
 import com.songpo.searched.entity.SlUser;
 import com.songpo.searched.mapper.SlReturnsDetailMapper;
 import com.songpo.searched.mapper.SlSignInMapper;
-import com.songpo.searched.service.LoginUserService;
-import com.songpo.searched.service.OrderDetailService;
-import com.songpo.searched.service.OrderService;
-import com.songpo.searched.service.ProcessOrders;
+import com.songpo.searched.service.*;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +16,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.entity.Example;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -39,16 +38,14 @@ public class CommonConfig {
     @Autowired
     private OrderDetailService orderDetailService;
     @Autowired
-    private OrderService orderService;
-    @Autowired
-    private ProcessOrders processOrders;
+    private UserService userService;
 //    @Autowired
 //    private WXPay wxPay;
 
     @Autowired
     private Environment env;
 
-    @Scheduled(cron = "0 0 0  * * ? ")   //每10秒执行一次
+    @Scheduled(cron = "0 0 0 * * *")
     public void aTask() {
         updSignState();
         updOrderPreSaleState();
@@ -61,17 +58,14 @@ public class CommonConfig {
      */
     void updSignState() {
         try {
-            Example example = new Example(SlSignIn.class);
-            example.createCriteria().andEqualTo("signNum", 7);
-            // 更新连续签到天数
-            int count = this.slSignInMapper.updateByExampleSelective(new SlSignIn() {{
-                setSignNum(0);
-            }}, example);
-            if (count == 1) {
-                SlUser user = loginUserService.getCurrentLoginUser();
-                this.slSignInMapper.delete(new SlSignIn() {{
-                    setUserId(user.getId());
-                }});
+            List<SlSignIn> signs = slSignInMapper.select(new SlSignIn() {{
+                setSignNum(7);
+            }});
+            for (SlSignIn sign : signs) {
+                Example example = new Example(SlSignIn.class);
+                example.createCriteria()
+                        .andEqualTo("userId", sign.getUserId());
+                this.slSignInMapper.deleteByExample(example);
             }
         } catch (Exception e) {
             log.error("更新失败", e);
@@ -82,11 +76,13 @@ public class CommonConfig {
      * 更新用户预售返现状态
      */
     void updOrderPreSaleState() {
-        LocalTime localTime = LocalTime.now();
-        localTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        today.format(df);
         List<SlReturnsDetail> list = this.returnsDetailMapper.selectAll();
         for (SlReturnsDetail detail : list) {
-            if (detail.getReturnTime().equals(localTime)) {
+            LocalDate ldt = LocalDate.parse(detail.getReturnTime().substring(0, 10), df);
+            if (ldt.equals(today)) {
                 returnsDetailMapper.updateByPrimaryKeySelective(new SlReturnsDetail() {{
                     setId(detail.getId());
                     // 当前时间和返现时间相同的话把状态改为可返
@@ -94,10 +90,8 @@ public class CommonConfig {
                 }});
             }
             if (detail.getReturnedStatus().equals(4)) {
-                DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                LocalDate ldt = LocalDate.parse(detail.getReturnTime(), df);
                 // 如果返现时间+3天 并且状态还是可返状态
-                if (ldt.plusDays(3).equals(localTime)) {
+                if (ldt.plusDays(3).equals(today)) {
                     // 把状态改为已逾期
                     returnsDetailMapper.updateByPrimaryKeySelective(new SlReturnsDetail() {{
                         setId(detail.getId());
@@ -109,24 +103,25 @@ public class CommonConfig {
     }
 
     /**
-     * 7天自动确认收货(普通商品)
+     * 7天自动确认收货
      */
     void updOrderConfirmReceipt() {
-        LocalTime localTime = LocalTime.now();
-        localTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         List<SlOrderDetail> detailList = this.orderDetailService.select(new SlOrderDetail() {{
             setShippingState(4);
         }});
         for (SlOrderDetail detail : detailList) {
-            DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            LocalDate ldt = LocalDate.parse(detail.getShippingTime(), df);
-            //如果发货时间 + 7天
-            if (ldt.plusDays(7).equals(localTime)) {
-                orderDetailService.updateByPrimaryKeySelective(new SlOrderDetail() {{
-                    setId(detail.getId());
-                    // 已完成/已收货
-                    setShippingState(5);
-                }});
+            if (StringUtils.isNotBlank(detail.getShippingTime())) {
+                LocalDate ldt = LocalDate.parse(detail.getShippingTime().substring(0, 10), df);
+                //如果发货时间 + 7天
+                if (ldt.plusDays(7).equals(today)) {
+                    orderDetailService.updateByPrimaryKeySelective(new SlOrderDetail() {{
+                        setId(detail.getId());
+                        // 已完成/已收货
+                        setShippingState(5);
+                    }});
+                }
             }
         }
 
@@ -158,48 +153,54 @@ public class CommonConfig {
      * 预售订单确认收货(商家发货3天,自动确认收货)
      */
     void updReturnsDetailOrderPreSaleState() {
-        Example e = new Example(SlReturnsDetail.class);
-        e.setOrderByClause("return_time ASC");
-        List<SlReturnsDetail> list = this.returnsDetailMapper.selectByExample(e);
+        Example example = new Example(SlReturnsDetail.class);
+        example.createCriteria()
+                //商家已返状态
+                .andEqualTo("returnedStatus", 1)
+                //但是用户并没有确认收货的
+                .andEqualTo("confirmReceipt", false);
+        List<SlReturnsDetail> list = this.returnsDetailMapper.selectByExample(example);
         for (SlReturnsDetail detail : list) {
             DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            LocalDateTime time = LocalDateTime.now();
-            LocalDateTime ldt = LocalDateTime.parse(detail.getReturnTime(), df);
+            LocalDate time = LocalDate.now();
+            LocalDate ldt = LocalDate.parse(detail.getReturnTime().substring(0, 10), df);
             Duration duration = Duration.between(ldt, time);
             if (duration.toDays() == 3) {
-                Example example = new Example(SlReturnsDetail.class);
-                example.createCriteria()
-                        .andEqualTo("id", detail.getId())
-                        //商家已返状态
-                        .andEqualTo("returnedStatus", 1)
-                        //但是用户并没有确认收货的
-                        .andEqualTo("confirmReceipt", false);
-                int count = returnsDetailMapper.updateByExampleSelective(new SlReturnsDetail() {{
-                    setConfirmReceipt(true);
-                }}, example);
-                if (count == 1) {
-                    if (detail.getId().equals(list.get(0).getId())) {
-                        Example example1 = new Example(SlOrderDetail.class);
-                        example1.createCriteria().andEqualTo("orderId", detail.getOrderId());
-                        this.orderDetailService.updateByExampleSelective(new SlOrderDetail() {{
+                Example example1 = new Example(SlReturnsDetail.class);
+                example1.setOrderByClause("return_time DESC");
+                example1.createCriteria()
+                        .andEqualTo("userId", detail.getOrderId())
+                        .andEqualTo("orderId", detail.getOrderId());
+                List<SlReturnsDetail> list1 = this.returnsDetailMapper.selectByExample(example1);
+                if (list1.size() > 0) {
+                    //如果本次确认收货的预售订单==最后一次返现的预售订单id
+                    if (list1.get(0).getId().equals(detail.getId())) {
+                        this.orderDetailService.updateByPrimaryKeySelective(new SlOrderDetail() {{
+                            setId(detail.getId());
                             // 已完成/未评价
                             setShippingState(5);
-                        }}, example1);
+                        }});
                         //把改订单号的所有订单更新为已完成状态
                         this.returnsDetailMapper.updateByExampleSelective(new SlReturnsDetail() {{
                             setReturnedStatus(5);
-                        }}, example);
+                            setConfirmReceipt(false);
+                        }}, example1);
+                    } else {
+                        this.returnsDetailMapper.updateByPrimaryKeySelective(new SlReturnsDetail() {{
+                            setId(detail.getId());
+                            //本期已返状态
+                            setConfirmReceipt(true);
+                        }});
                     }
                 }
             }
         }
-    }
 
-    /**
-     * 更新微信支付订单支付状态
-     * 定时执行方法，调用微信订单查询接口，查询订单支付状态，并处理系统订单
-     * 来源 https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_2&index=4
-     */
+        /**
+         * 更新微信支付订单支付状态
+         * 定时执行方法，调用微信订单查询接口，查询订单支付状态，并处理系统订单
+         * 来源 https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_2&index=4
+         */
 //    @Scheduled(cron = "0/10 * *  * * ? ")
 //    void updateWXPayStatus() {
 //        List<SlOrder> orderList = orderService.select(new SlOrder() {{
@@ -247,4 +248,4 @@ public class CommonConfig {
 //            }
 //        }
 //    }
-}
+    }
