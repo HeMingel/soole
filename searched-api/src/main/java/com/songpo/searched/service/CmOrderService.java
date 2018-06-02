89b8dd136cf8a3c6351762e55774502fa6000ede
王晓森 <you@example.com>
2018-06-02 16:58:57 +0800

    修复 预售确认收货BUG

--- searched-api/src/main/java/com/songpo/searched/service/CmOrderService.java
+++ searched-api/src/main/java/com/songpo/searched/service/CmOrderService.java
@@ -292,20 +292,6 @@ public class CmOrderService {
                                                             setPlaceOrderReturnPulse(repository.getPlaceOrderReturnPulse() * quantity);
                                                         }
                                                     }});
-                                                    // 忘了这块是要表达什么 ?? 如果是新人专享活动的话
-//                                        if (slActivityProduct.getActivityId().equals(ActivityConstant.NEW_PEOPLE_ACTIVITY)) {
-//                                            Example example1 = new Example(SlActivityProduct.class);
-//                                            example1.createCriteria()
-//                                                    .andEqualTo("productId", slProduct.getId())
-//                                                    .andGreaterThan("count", 0);
-//                                            this.activityProductMapper.updateByExampleSelective(new SlActivityProduct() {{
-//                                                if (activityProductCount == 0) {
-//                                                    setEnabled(false);
-//                                                }
-//                                                //活动总商品上架数量 - 本次购买的数量
-//                                                setCount(activityProductCount);
-//                                            }}, example1);
-//                                        } else {}
                                                     // 如果库存为0 的话就下架了
                                                     Example example = new Example(SlActivityProduct.class);
                                                     example.createCriteria()
@@ -863,21 +849,6 @@ public class CmOrderService {
         slOrder.setUserId(userId);
         // 团主Id
         slOrder.setGroupMaster(groupMaster);
-        // 订单类型
-//        slOrder.setType(type);
-        // 如果是拼团订单
-//        if (Integer.parseInt(slProduct.getSalesModeId()) == SalesModeConstant.SALES_MODE_GROUP) {
-//            // 查询该订单号的所有订单 && 支付成功状态
-//            int count2 = this.orderService.selectCount(new SlOrder() {{
-//                setSerialNumber(serialNumber);
-//                setPaymentState(1);
-//            }});
-//            // 如果单数 + 他自己 <=所需人数
-//            if (count2 + 1 <= activityProduct.getPeopleNum()) {
-//                // 拼团状态为拼团中状态
-//                slOrder.setSpellGroupStatus(1);
-//            }
-//        }
         // 该商品的规格价格 * 加入购物车中的数量 = 该用户本次加入商品的价格
         double price = 0.00;
         if (spellGroupType == 1) {
@@ -1311,8 +1282,10 @@ public class CmOrderService {
             // 查询有没有这个订单
             SlOrderDetail detail = this.orderDetailService.selectOne(new SlOrderDetail() {{
                 setOrderId(orderId);
-                //并且是待发货状态
-                setShippingState(3);
+                //并且是已发货状态
+                setShippingState(4);
+                //用户id
+                setCreator(user.getId());
             }});
             // 有这个订单明细
             if (null != detail) {
