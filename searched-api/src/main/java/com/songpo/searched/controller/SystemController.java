210a5f20e44a789f95c4cb63fe64f14b5ebe609e
何鸣 <angelhm@sina.cn>
2018-07-14 15:45:45 +0800

    合并账号豆的正确转移

--- searched-api/src/main/java/com/songpo/searched/controller/SystemController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/SystemController.java
@@ -737,7 +737,7 @@ public class SystemController {
                         phoneUser.setType(user.getType());
                         //修补错误数据，原来的微信用户中的金豆银豆转移
                         phoneUser.setSilver(phoneUser.getSilver() + user.getSilver());
-                        phoneUser.setCoin(phoneUser.getCoin() + ((user.getCoin()-BaseConstant.REGISTER_PEAS)<0?0:(user.getCoin()-BaseConstant.REGISTER_PEAS))); 
+                        phoneUser.setCoin(phoneUser.getCoin() + ((user.getCoin()-BaseConstant.REGISTER_PEAS)<0?0:(user.getCoin()-BaseConstant.REGISTER_PEAS)));
                         // 更新
                         userService.updateByPrimaryKeySelective(phoneUser);
                         // 更新缓存
@@ -855,7 +855,7 @@ public class SystemController {
                         phoneUser.setZone(zone);
                         phoneUser.setType(user.getType());
                         //修补错误数据，原来的微信用户中的金豆银豆转移
-                        phoneUser.setSilver(phoneUser.getSilver() + ((user.getSilver() - 100) < 0 ? 0 : (user.getSilver() - 100)));
+                        phoneUser.setCoin(phoneUser.getCoin() + ((user.getCoin()-BaseConstant.REGISTER_PEAS)<0?0:(user.getCoin()-BaseConstant.REGISTER_PEAS)));
                         phoneUser.setCoin(phoneUser.getCoin() + user.getCoin());
                         // 更新
                         userService.updateByPrimaryKeySelective(phoneUser);
