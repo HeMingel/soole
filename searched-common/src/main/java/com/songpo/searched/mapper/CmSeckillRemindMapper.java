298d62f9a1cea32d1e1801c7396a6c332fa0aaab
何鸣 <angelhm@sina.cn>
2018-07-27 14:30:33 +0800

    修改城市非必选

--- searched-common/src/main/java/com/songpo/searched/mapper/CmSeckillRemindMapper.java
+++ searched-common/src/main/java/com/songpo/searched/mapper/CmSeckillRemindMapper.java
@@ -4,22 +4,13 @@ import com.songpo.searched.entity.SlSeckillRemind;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 @Mapper
 public interface CmSeckillRemindMapper {
     //查询提醒列表
     List<SlSeckillRemind> listRemind (@Param("remindTime") String remindTime);
-import io.lettuce.core.dynamic.annotation.Param;
-import org.apache.ibatis.annotations.Mapper;
-
-import java.util.Date;
-
-/**
- * @author yinsl
- */
-@Mapper
-public interface CmSeckillRemindMapper {
 
     /**
      *  添加限时抢购提醒
