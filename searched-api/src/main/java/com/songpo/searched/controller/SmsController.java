ce8022bacffc7c1f5acd7c2f67f9e26370d5d90e
yinsl <ysltechnology@163.com>
2018-08-14 11:57:04 +0800

    手动成功登录验证码

--- searched-api/src/main/java/com/songpo/searched/controller/SmsController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/SmsController.java
@@ -1,6 +1,8 @@
 package com.songpo.searched.controller;
 
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.songpo.searched.cache.SmsPasswordCache;
+import com.songpo.searched.cache.SmsVerifyCodeCache;
 import com.songpo.searched.domain.BusinessMessage;
 import com.songpo.searched.service.SmsService;
 import io.swagger.annotations.Api;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author 刘松坡
  */
@@ -24,6 +28,11 @@ public class SmsController {
 
     @Autowired
     private SmsService smsService;
+    @Autowired
+    private SmsVerifyCodeCache smsVerifyCodeCache;
+
+    @Autowired
+    private SmsPasswordCache smsPasswordCache;
 
     @ApiOperation(value = "发送短信验证码")
     @ApiImplicitParams({
@@ -43,5 +52,27 @@ public class SmsController {
     @PostMapping("pwd")
     public BusinessMessage<SendSmsResponse> sendPassword(String mobile, String zone) {
         return smsService.sendPassword(mobile, zone);
+    }
+
+    @ApiOperation(value = "手动生成验证码")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "mobile", value = "手机号码", paramType = "form"),
+            @ApiImplicitParam(name = "zone", value = "地区", paramType = "form")
+    })
+    @PostMapping("code")
+    public BusinessMessage sendCode(String mobile) {
+        BusinessMessage businessMessage = new BusinessMessage();
+        String [] strings = mobile.split(",");
+        try {
+            for (int i = 0; i<strings.length; i++){
+                this.smsPasswordCache.put(strings[i], "123456", 120L, TimeUnit.MINUTES);
+            }
+            businessMessage.setMsg("生成验证码成功");
+            businessMessage.setSuccess(true);
+        }catch (Exception e){
+            businessMessage.setMsg(e.toString());
+            businessMessage.setSuccess(false);
+        }
+       return businessMessage;
     }
 }
