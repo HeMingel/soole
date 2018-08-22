f403f5355373d1aed89caff9347ee54bb18e3621
何鸣 <angelhm@sina.cn>
2018-08-22 10:12:39 +0800

    清理项目代码

--- searched-api/src/main/java/com/songpo/searched/controller/ProductController.java
+++ searched-api/src/main/java/com/songpo/searched/controller/ProductController.java
@@ -96,13 +96,15 @@ public class ProductController {
                                                                             String synthesize) {
         log.debug("分页查询商品，名称：{}，销售模式唯一标识符：{}，商品分类唯一标识符：{}，商品分类标识(一级二级):{},最小经度：{}，最大经度：{}，最小维度：{}，" +
                         "最大维度：{}，按商品价格排序规则：{}，按店铺评分排序规则：{}，价格区间最小值：{}，价格区间最大值：{}，页码：{}，" +
-                        "容量：{},销售数量排序:{},用户当前位置:{},当前经度:{},当前纬度:{},综合排序:{}", name, salesModeId, activityId, goodsTypeId, longitudeMin, longitudeMax, latitudeMin,
-                latitudeMax, sortByPrice, sortByRating, priceMin, priceMax, pageNum, pageSize, sortBySale, addressNow, longitudeNow, latitudeNow,synthesize);
+                        "容量：{},销售数量排序:{},用户当前位置:{},当前经度:{},当前纬度:{},综合排序:{}", name, salesModeId, activityId, goodsTypeId,
+                longitudeMin, longitudeMax, latitudeMin, latitudeMax, sortByPrice, sortByRating, priceMin, priceMax, pageNum,
+                pageSize, sortBySale, addressNow, longitudeNow, latitudeNow,synthesize);
         BusinessMessage<PageInfo<Map<String, Object>>> message = new BusinessMessage<>();
         message.setSuccess(false);
         try {
-            PageInfo data = this.productService.selectBySalesMode(name, salesModeId, activityId, goodsTypeId, goodsTypeStatus, longitudeMin, longitudeMax, latitudeMin,
-                    latitudeMax, sortByPrice, sortByRating, priceMin, priceMax, pageNum, pageSize, sortBySale, addressNow, longitudeNow, latitudeNow,synthesize);
+            PageInfo data = this.productService.selectBySalesMode(name, salesModeId, activityId, goodsTypeId, goodsTypeStatus, longitudeMin, longitudeMax,
+                    latitudeMin,latitudeMax, sortByPrice, sortByRating, priceMin, priceMax, pageNum, pageSize, sortBySale, addressNow, longitudeNow,
+                    latitudeNow,synthesize);
 
             message.setData(data);
             message.setSuccess(true);
